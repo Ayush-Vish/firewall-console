2016fc4155655b96b00306dd1ca7bdefef3059ac
Ayush Vishwwakarma <ayushvish6555@gmail.com>
2024-09-09 01:10:28 +0530

    🐛 Removed TUrborepo

--- packages/db/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./mongo/connect"
-export * from "./models/user.model";
-export * from "./models/endpoint.model"
-export * from "./models/policy.model";
-export * from "./models/anamoly.model";
